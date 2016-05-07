9ca6b815cb7ffe5e0473dbe4c2c78cdccd407b2c
Alex Wang <alex3763@gmail.com>
2016-05-06 21:38:24 -0400

    setup api stuff

---
+++ main.py
@@ -0,0 +1,3 @@
+import twitter
+api = twitter.Api(consumer_key="TyxYMC4JSNwdiOnrhdDCeczYu",consumer_secret="gtld0ZFiEmrRn9GKoL8SvYfYXOpBHG479VYlvQBBhqw7fBR1eK",access_token_key="728756728108601344-tVBswjzuMn3s2k3tcq1q1ZaQJigkkh8",access_token_secret="Eo9n54135QPApXBaUJ5i3VaYbTd3rPnBGSMPWvsW4iQQl")
+
